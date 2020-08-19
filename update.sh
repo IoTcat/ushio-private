cfedd343d262bb3b812eb774cc977cc173d2a175
iotcat <i@iotcat.me>
2020-08-19 21:55:57 +0800

    ::

--- update.sh
+++ update.sh
@@ -3,3 +3,7 @@ git submodule update --init --recursive
 git submodule foreach git submodule update --init --recursive
 git submodule update
 git submodule foreach git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull
+git submodule foreach git submodule foreach git checkout master
+git submodule foreach git submodule foreach git pull
